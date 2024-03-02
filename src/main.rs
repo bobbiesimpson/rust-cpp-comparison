de6b3a72b30995aa800cd0654e75aabec37dac1c
Rob Simpson <robertnsimpson@gmail.com>
2024-03-02 14:23:09 +0000

    Add rulinalg dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use rulinalg::matrix::Matrix;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Performing matrix algebra tests...");
+    let a = Matrix::new(2, 2, vec![1.0, 2.0, 3.0, 4.0]);
+    println!("Finished.")
 }
