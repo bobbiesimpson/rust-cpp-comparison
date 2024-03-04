72a10c7529e411241e7502d6246af2e8c3af3d00
Rob Simpson <robertnsimpson@gmail.com>
2024-03-04 21:12:12 +0000

    Simple number guess game to get used to Rust syntax

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
-use rulinalg::matrix::Matrix;
+use std::io;
+use rand::Rng;
 
 fn main() {
-    println!("Performing matrix algebra tests...");
-    let a = Matrix::new(2, 2, vec![1.0, 2.0, 3.0, 4.0]);
-    println!("Finished.")
+
+    let lower_bound = 1;
+    let upper_bound = 100;
+    let n = rand::thread_rng().gen_range(lower_bound..=upper_bound);
+    let mut int_guess = -1;
+
+    println!("Guess the number between {lower_bound} and {upper_bound}");
+    while int_guess != n {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read string");
+        int_guess = guess.trim().parse::<i32>().unwrap();
+
+        if int_guess > n {
+            println!("Too high");
+        }
+        else if int_guess < n {
+            println!("Too low");
+        }
+        else {
+            println!("Bang on!")
+        }
+    }
 }
