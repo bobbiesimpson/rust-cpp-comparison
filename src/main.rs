2b4058c0bfdfa093a4cbcc68c974b8b278ff4659
Rob Simpson <robertnsimpson@gmail.com>
2024-03-06 22:25:00 +0000

    Replace if statements with match expression

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,26 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
 
     let lower_bound = 1;
     let upper_bound = 100;
     let n = rand::thread_rng().gen_range(lower_bound..=upper_bound);
-    let mut int_guess = -1;
 
     println!("Guess the number between {lower_bound} and {upper_bound}");
-    while int_guess != n {
+    loop {
         let mut guess = String::new();
         io::stdin().read_line(&mut guess).expect("Failed to read string");
-        int_guess = guess.trim().parse::<i32>().unwrap();
+        let int_guess = guess.trim().parse::<i32>().unwrap();
 
-        if int_guess > n {
-            println!("Too high");
-        }
-        else if int_guess < n {
-            println!("Too low");
-        }
-        else {
-            println!("Bang on!")
+        match int_guess.cmp(&n) {
+            Ordering::Less => println!("Too low"),
+            Ordering::Greater => println!("Too high"),
+            Ordering::Equal => {
+                println!("Bang on!");
+                break;  // break loop
+            }
         }
     }
 }
